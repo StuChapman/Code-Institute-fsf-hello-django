7f731c5b1fc5184cf5ba7b06b0e76259910993c4
Stuart Chapman <chapman.stuart@sky.com>
2021-01-09 11:58:28 +0000

    Build: added development variable to gitpod settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import dj_database_url
 
-development = os.development.get('DEVELOPMENT', False)
+development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,9 +82,9 @@ if development:
         }
     }
 else:
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 # Password validation
